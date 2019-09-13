429b894cdbe3ec840535da9dcca0bf2ae5f6cae1
vladimir.kopso <vladimir.kopso@solarwinds.cloud>
2019-09-13 09:46:00 +0200

    fixed tello_control.ui.py

--- Tello_Video_With_Pose_Recognition/tello_control_ui.py
+++ Tello_Video_With_Pose_Recognition/tello_control_ui.py
@@ -1,19 +1,20 @@
 from PIL import Image
 from PIL import ImageTk
-import Tkinter as tki
-from Tkinter import Toplevel, Scale
+import tkinter as tki
+from tkinter import Toplevel, Scale
 import threading
 import datetime
 import cv2
 import os
 import time
-from tello_pose import Tello_Pose
+from .tello_pose import Tello_Pose
 import platform
+
 
 class TelloUI:
     """Wrapper class to enable the GUI."""
 
-    def __init__(self,tello,outputpath):
+    def __init__(self, tello, outputpath):
         """
         Initial all the element of the GUI,support by Tkinter
 
@@ -21,36 +22,37 @@ class TelloUI:
 
         Raises:
             RuntimeError: If the Tello rejects the attempt to enter command mode.
-        """        
+        """
 
-        self.tello = tello # videostream device
-        self.outputPath = outputpath # the path that save pictures created by clicking the takeSnapshot button 
+        self.tello = tello  # videostream device
+        self.outputPath = outputpath  # the path that save pictures created by clicking the takeSnapshot button
         self.frame = None  # frame read from h264decoder and used for pose recognition 
-        self.thread = None # thread of the Tkinter mainloop
-        self.stopEvent = None  
-        
+        self.thread = None  # thread of the Tkinter mainloop
+        self.stopEvent = None
+
         # control variables
         self.distance = 0.1  # default distance for 'move' cmd
         self.degree = 30  # default degree for 'cw' or 'ccw' cmd
         # if the pose recognition mode is opened 
-        self.pose_mode = False        
+        self.pose_mode = False
         # if the flag is TRUE,the auto-takeoff thread will stop waiting for the response from tello
         self.quit_waiting_flag = False
-        
+
         # if the flag is TRUE,the pose recognition skeleton will be drawn on the GUI picture
         self.draw_skeleton_flag = False
         # pose recognition
         self.my_tello_pose = Tello_Pose()
-        
+
         # record the coordinates of the nodes in the pose recognition skeleton     
         self.points = []
-        #list of all the possible connections between skeleton nodes        
-        self.POSE_PAIRS = [[0,1], [1,2], [2,3], [3,4], [1,5], [5,6], [6,7], [1,14], [14,8], [8,9], [9,10], [14,11], [11,12], [12,13] ]
-        
+        # list of all the possible connections between skeleton nodes
+        self.POSE_PAIRS = [[0, 1], [1, 2], [2, 3], [3, 4], [1, 5], [5, 6], [6, 7], [1, 14], [14, 8], [8, 9], [9, 10], [14, 11], [11, 12],
+                           [12, 13]]
+
         # initialize the root window and image panel
         self.root = tki.Tk()
         self.panel = None
-       # self.panel_for_pose_handle_show = None
+        # self.panel_for_pose_handle_show = None
 
         # create buttons
         self.btn_snapshot = tki.Button(self.root, text="Snapshot!",
@@ -71,7 +73,7 @@ class TelloUI:
             self.root, text="Open Command Panel", relief="raised", command=self.openCmdWindow)
         self.btn_landing.pack(side="bottom", fill="both",
                               expand="yes", padx=10, pady=5)
-        
+
         # start a thread that constantly pools the video sensor for
         # the most recently read frame
         self.stopEvent = threading.Event()
@@ -85,8 +87,9 @@ class TelloUI:
         # the auto-takeoff thread will start if the 'takeoff' button on command window is clicked 
         self.auto_takeoff_thread = threading.Thread(target=self._autoTakeoff)
         # the sending_command will send command to tello every 5 seconds
-        self.sending_command_thread = threading.Thread(target = self._sendingCommand)
-        self.get_GUI_Image_thread = threading.Thread(target = self._getGUIImage)
+        self.sending_command_thread = threading.Thread(target=self._sendingCommand)
+        self.get_GUI_Image_thread = threading.Thread(target=self._getGUIImage)
+
     def videoLoop(self):
         """
         The mainloop thread of Tkinter 
@@ -97,75 +100,76 @@ class TelloUI:
             # start the thread that get GUI image and drwa skeleton 
             time.sleep(0.5)
             self.get_GUI_Image_thread.start()
-            while not self.stopEvent.is_set():                
+            while not self.stopEvent.is_set():
                 # read the frame for pose recognition
-                self.frame = self.tello.read()                
+                self.frame = self.tello.read()
                 if self.frame is None or self.frame.size == 0:
                     continue
                 # smoothing filter
-                self.frame = cv2.bilateralFilter(self.frame, 5, 50, 100)  
+                self.frame = cv2.bilateralFilter(self.frame, 5, 50, 100)
 
                 cmd = ''
-                self.points.append(None)               
+                self.points.append(None)
                 # process pose-recognition                
                 if self.pose_mode:
-                    cmd,self.draw_skeleton_flag,self.points = self.my_tello_pose.detect(self.frame)
-            
+                    cmd, self.draw_skeleton_flag, self.points = self.my_tello_pose.detect(self.frame)
+
                 # process command - map your motion to whatever Tello movement you want!
                 if cmd == 'moveback':
                     self.telloMoveBackward(0.50)
                 elif cmd == 'moveforward':
-                    self.telloMoveForward(0.50) 
+                    self.telloMoveForward(0.50)
                 elif cmd == 'land':
                     self.telloLanding()
-                                            
-        except RuntimeError, e:
+
+        except RuntimeError as e:
             print("[INFO] caught a RuntimeError")
-    
+
     def _getGUIImage(self):
         """
         Main operation to read frames from h264decoder and draw skeleton on 
         frames if the pose mode is opened
-        """  
+        """
         # read the system of your computer
         system = platform.system()
         while not self.stopEvent.is_set():
             # read the frame for GUI show
             frame = self.tello.read()
             if frame is None or frame.size == 0:
-                continue 
+                continue
             if self.pose_mode:
                 # Draw the detected skeleton points
                 for i in range(15):
                     if self.draw_skeleton_flag == True:
                         cv2.circle(frame, self.points[i], 8, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
-                        cv2.putText(frame, "{}".format(i), self.points[i], cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2,lineType=cv2.LINE_AA)       
-                # Draw Skeleton
+                        cv2.putText(frame, "{}".format(i), self.points[i], cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2,
+                                    lineType=cv2.LINE_AA)
+                        # Draw Skeleton
                 for pair in self.POSE_PAIRS:
                     partA = pair[0]
                     partB = pair[1]
                     if self.points[partA] and self.points[partB]:
                         cv2.line(frame, self.points[partA], self.points[partB], (0, 255, 255), 2)
                         cv2.circle(frame, self.points[partA], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
-            
+
             # transfer the format from frame to image         
             image = Image.fromarray(frame)
 
             # we found compatibility problem between Tkinter,PIL and Macos,and it will 
             # sometimes result the very long preriod of the "ImageTk.PhotoImage" function,
             # so for Macos,we start a new thread to execute the _updateGUIImage function.
-            if system =="Windows" or system =="Linux":                
+            if system == "Windows" or system == "Linux":
                 self._updateGUIImage(image)
 
             else:
-                thread_tmp = threading.Thread(target=self._updateGUIImage,args=(image,))
+                thread_tmp = threading.Thread(target=self._updateGUIImage, args=(image,))
                 thread_tmp.start()
-                time.sleep(0.03) 
-           
-    def _updateGUIImage(self,image):
+                time.sleep(0.03)
+
+    def _updateGUIImage(self, image):
         """
         Main operation to initial the object of image,and update the GUI panel 
-        """  
+        """
         image = ImageTk.PhotoImage(image)
         # if the panel none ,we need to initial it
         if self.panel is None:
@@ -192,68 +196,68 @@ class TelloUI:
         normal range,tello will execute the moveup command.Otherwise,tello will land.
         
         Finally,the sending-command thread will start.
-        """ 
-        response = None 
-        height_tmp = 0 # temp variable to content value of height
-        height_val = 0 # average value of height
-        cnt = 0        # effective number of height reading
-        timeout = 6    # max waiting time of tello's response
+        """
+        response = None
+        height_tmp = 0  # temp variable to content value of height
+        height_val = 0  # average value of height
+        cnt = 0  # effective number of height reading
+        timeout = 6  # max waiting time of tello's response
 
         timer = threading.Timer(timeout, self._setQuitWaitingFlag)
-        timer.start()        
+        timer.start()
 
         # waiting for the response from tello
-        while response != 'ok' :
+        while response != 'ok':
             if self.quit_waiting_flag is True:
                 break
             response = self.tello.get_response()
-            print "ack:%s"%response 
+            print(f"ack: {response}")
         timer.cancel()
 
         # receive the correct response
         if response == 'ok':
             self.tello.move_up(0.5)
-        
+
         # calculate the height of tello
         else:
-            for i in range(0,50):
+            for i in range(0, 50):
                 height_tmp = self.tello.get_height()
                 try:
                     height_val = height_val + height_tmp
                     cnt = cnt + 1
-                    print height_tmp,cnt
+                    print(height_tmp, cnt)
                 except:
                     height_val = height_val
-                
-            height_val = height_val/cnt
-            
+
+            height_val = height_val / cnt
+
             # if the height value is in normal range 
             if height_val == 9 or height_val == 10 or height_val == 11:
-                self.tello.move_up(0.5)    
+                self.tello.move_up(0.5)
             else:
                 self.tello.land()
         # start the sendingCmd thread       
         self.sending_command_thread.start()
-            
+
     def _sendingCommand(self):
         """
         start a while loop that sends 'command' to tello every 5 second
-        """    
+        """
 
         while True:
-            self.tello.send_command('command')        
+            self.tello.send_command('command')
             time.sleep(5)
 
-    def _setQuitWaitingFlag(self):  
+    def _setQuitWaitingFlag(self):
         """
         set the variable as TRUE,it will stop computer waiting for response from tello  
-        """       
-        self.quit_waiting_flag = True        
-   
+        """
+        self.quit_waiting_flag = True
+
     def openCmdWindow(self):
         """
         open the cmd window and initial all the button and text
-        """        
+        """
         panel = Toplevel(self.root)
         panel.wm_title("Command Panel")
 
@@ -266,10 +270,10 @@ class TelloUI:
         text0.pack(side='top')
 
         text1 = tki.Label(panel, text=
-                          'W - Move Tello Up\t\t\tArrow Up - Move Tello Forward\n'
-                          'S - Move Tello Down\t\t\tArrow Down - Move Tello Backward\n'
-                          'A - Rotate Tello Counter-Clockwise\tArrow Left - Move Tello Left\n'
-                          'D - Rotate Tello Clockwise\t\tArrow Right - Move Tello Right',
+        'W - Move Tello Up\t\t\tArrow Up - Move Tello Forward\n'
+        'S - Move Tello Down\t\t\tArrow Down - Move Tello Backward\n'
+        'A - Rotate Tello Counter-Clockwise\tArrow Left - Move Tello Left\n'
+        'D - Rotate Tello Clockwise\t\tArrow Right - Move Tello Right',
                           justify="left")
         text1.pack(side="top")
 
@@ -324,7 +328,7 @@ class TelloUI:
         """
         open the flip window and initial all the button and text
         """
-        
+
         panel = Toplevel(self.root)
         panel.wm_title("Gesture Recognition")
 
@@ -347,7 +351,7 @@ class TelloUI:
             panel, text="Flip Backward", relief="raised", command=self.telloFlip_b)
         self.btn_flipb.pack(side="bottom", fill="both",
                             expand="yes", padx=10, pady=5)
-       
+
     def takeSnapshot(self):
         """
         save the current frame of the video as a jpg file and put it into outputpath
@@ -401,9 +405,9 @@ class TelloUI:
         takeoff_response = self.tello.get_response()
 
         if takeoff_response != 'error':
-            self.auto_takeoff_thread.start()       
+            self.auto_takeoff_thread.start()
         else:
-            print "battery low,please repalce with a new one"                          
+            print("Battery low, please replace with a new one")
 
     def telloLanding(self):
         return self.tello.land()
@@ -449,42 +453,42 @@ class TelloUI:
 
     def updateDistancebar(self):
         self.distance = self.distance_bar.get()
-        print 'reset distance to %.1f' % self.distance
+        print(f'reset distance to {self.distance}')
 
     def updateDegreebar(self):
         self.degree = self.degree_bar.get()
-        print 'reset distance to %d' % self.degree
+        print(f'reset distance to {self.degree}')
 
     def on_keypress_w(self, event):
-        print "up %d m" % self.distance
+        print(f"up {self.distance} m")
         self.telloUp(self.distance)
 
     def on_keypress_s(self, event):
-        print "down %d m" % self.distance
+        print(f"down {self.distance} m")
         self.telloDown(self.distance)
 
     def on_keypress_a(self, event):
-        print "ccw %d degree" % self.degree
+        print(f"ccw {self.degree} degree")
         self.tello.rotate_ccw(self.degree)
 
     def on_keypress_d(self, event):
-        print "cw %d m" % self.degree
+        print(f"cw {self.degree} m")
         self.tello.rotate_cw(self.degree)
 
     def on_keypress_up(self, event):
-        print "forward %d m" % self.distance
+        print(f"forward {self.distance} m")
         self.telloMoveForward(self.distance)
 
     def on_keypress_down(self, event):
-        print "backward %d m" % self.distance
+        print(f"backward {self.distance} m")
         self.telloMoveBackward(self.distance)
 
     def on_keypress_left(self, event):
-        print "left %d m" % self.distance
+        print(f"left {self.distance} m")
         self.telloMoveLeft(self.distance)
 
     def on_keypress_right(self, event):
-        print "right %d m" % self.distance
+        print(f"right {self.distance} m")
         self.telloMoveRight(self.distance)
 
     def on_keypress_enter(self, event):
@@ -502,4 +506,3 @@ class TelloUI:
         self.stopEvent.set()
         del self.tello
         self.root.quit()
-
