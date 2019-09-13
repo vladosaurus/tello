485ec7ac6de61f7dfa4debb4daa4fe3f0bd384ab
vladimir.kopso <vladimir.kopso@solarwinds.cloud>
2019-09-13 08:37:09 +0200

    tello_test.py to Python 3

--- Single_Tello_Test/tello_test.py
+++ Single_Tello_Test/tello_test.py
@@ -1,4 +1,4 @@
-from tello import Tello
+from .tello import Tello
 import sys
 from datetime import datetime
 import time
@@ -17,7 +17,7 @@ for command in commands:
 
         if command.find('delay') != -1:
             sec = float(command.partition('delay')[2])
-            print 'delay %s' % sec
+            print(f'delay {sec}')
             time.sleep(sec)
             pass
         else:
