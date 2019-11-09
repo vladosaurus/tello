7b0aed8b2dac3a9aa978ff47a6d7b11749d8fc4a
Štěpán Odstrčil <stepan.odstrcil@puxdesign.cz>
2019-11-09 14:15:51 +0100

    Copy correct GUI in the right folder

--- PySide2GUI/ui/WubaGUI.py
+++ PySide2GUI/ui/WubaGUI.py
@@ -3,7 +3,7 @@
 # Form implementation generated from reading ui file 'PySide2GUI/ui/WubaGUI.ui',
 # licensing of 'PySide2GUI/ui/WubaGUI.ui' applies.
 #
-# Created: Sat Nov  9 12:11:20 2019
+# Created: Sat Nov  9 14:15:32 2019
 #      by: pyside2-uic  running on PySide2 5.13.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -13,22 +13,12 @@ from PySide2 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(958, 719)
+        MainWindow.resize(800, 720)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 958, 21))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QtWidgets.QApplication.translate("MainWindow", "Wubalubadubdub", None, -1))
-        MainWindow.setToolTip(QtWidgets.QApplication.translate("MainWindow", "Pickle Riiiiiick!", None, -1))
-
+        self.gridLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(0, 0, 799, 719))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.gRoot = QtWidgets.QGridLayout(self.gridLayoutWidget)
+        self.gRoot.setContentsMargins(-1, -1, -1, 21)
+        self.gRoot.setObjectName("gRoot")
