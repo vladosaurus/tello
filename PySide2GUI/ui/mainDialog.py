0567ac4d623bbef5a6b9747d5b797db31300a229
Štěpán Odstrčil <stepan.odstrcil@puxdesign.cz>
2019-11-09 21:36:49 +0100

    Changes

--- PySide2GUI/ui/mainDialog.py
+++ PySide2GUI/ui/mainDialog.py
@@ -1,11 +1,11 @@
 from ui.WubaGUI import Ui_MainWindow
 from PySide2 import QtCore, QtGui, QtWidgets
 
-from Single_Tello_Test.dr2 import movements as go
-from Single_Tello_Test.dr2 import angles as degrees
+from ...Single_Tello_Test.dr2 import movements as go
+from ...Single_Tello_Test.dr2 import angles as degrees
 
 # import PyPlot widget for our 3D plot
-# from ..plot.plot import PlotWidget
+from ..plot.plot import PlotWidget
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -13,18 +13,18 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     VERTICAL_MOVE = 30
     ROTATION_MOVE = 30
 
-
     def __init__(self):
         # This is equivalent to super(self.__class__, self).__init__()
         super().__init__()
 
         # Commands
-        self.commands = {"go", "back", "forward", "cw", "ccw", "takeoff", "land"}
+        self.commands = { "go", "back", "forward", "cw", "ccw", "takeoff", "land" }
+        self._flying = False
 
         self.setupUi(self)
 
         # Any changes to layout, etc. has to be done after UI setup
-        # self.gRoot.addWidget(self.plot, 1, 1, 1, 1)
+        self.gRoot.addWidget(self.plot, 1, 1, 1, 1)
     
     # ------------------------
     # PROPERTIES
@@ -33,14 +33,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     @property
     def command_text(self):
         return self.lineEdit.text()
-
-    @property
-    def flying(self):
-        return True
-
-    @flying.setter
-    def flying(self, flying):
-        self.flying = flying
 
     # ------------------------
     # METHODS
@@ -54,29 +46,28 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     # ------------------------
 
     def on_turn_left(self):
-        self.teCommands.appendPlainText(degrees("ccw", self.ROTATION_MOVE))
+        self.append_command(degrees("ccw", self.ROTATION_MOVE))
 
     def on_turn_right(self):
-        self.teCommands.appendPlainText(degrees("cw", self.ROTATION_MOVE))
+        self.append_command(degrees("cw", self.ROTATION_MOVE))
 
     def on_forward(self):
-        self.teCommands.appendPlainText(go(self.HORIZONTAL_MOVE, 0, 0, self.leSpeed.text()))
+        self.append_command(go(self.HORIZONTAL_MOVE, 0, 0, self.leSpeed.text()))
 
     def on_back(self):
-        self.teCommands.appendPlainText(go(-self.HORIZONTAL_MOVE, 0, 0, self.leSpeed.text()))
+        self.append_command(go(-self.HORIZONTAL_MOVE, 0, 0, self.leSpeed.text()))
 
     def on_strafe_left(self):
-        self.teCommands.appendPlainText(go(0, -self.HORIZONTAL_MOVE, 0, self.leSpeed.text()))
+        self.append_command(go(0, -self.HORIZONTAL_MOVE, 0, self.leSpeed.text()))
 
     def on_strafe_right(self):
-        self.teCommands.appendPlainText(go(0, self.HORIZONTAL_MOVE, 0, self.leSpeed.text()))
+        self.append_command(go(0, self.HORIZONTAL_MOVE, 0, self.leSpeed.text()))
 
     def on_down(self):
-        self.teCommands.appendPlainText(go(0, 0, -self.VERTICAL_MOVE, self.leSpeed.text()))
+        self.append_command(go(0, 0, -self.VERTICAL_MOVE, self.leSpeed.text()))
 
     def on_up(self):
-        # self.plot.fly_up()
-        self.teCommands.appendPlainText(go(0, 0, self.VERTICAL_MOVE, self.leSpeed.text()))
+        self.append_command(go(0, 0, self.VERTICAL_MOVE, self.leSpeed.text()))
 
     def on_add_command(self):
         command = self.command_text.split(" ")[0]
@@ -86,7 +77,14 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         raise ValueError('Non-existing command!')
 
     def on_takeoff_land(self):
-        print("on_takeoff_land")
+        self._flying = not self._flying
+
+        if self._flying:
+            self.bTakeOffLand_2.text("Land")
+            self.append_command("takeoff")
+        else:
+            self.bTakeOffLand_2.text("Take Off")
+            self.append_command("land")
 
     def on_pitch_up(self):
         print("on_pitch_up")
